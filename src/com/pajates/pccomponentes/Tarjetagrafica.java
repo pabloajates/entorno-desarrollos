79a9dd67aa823d317dada02e7d694c82f3b5bdc1
pabloajates <115726046+pabloajates@users.noreply.github.com>
2022-10-17 19:05:24 +0300

    Clase tarjeta grafica

--- src/com/pajates/pccomponentes/Tarjetagrafica.java
+++ src/com/pajates/pccomponentes/Tarjetagrafica.java
@@ -1,4 +1,42 @@
 package com.pajates.pccomponentes;
 
 public class Tarjetagrafica {
-}
+
+    private Integer id;
+    private String marca;
+    private String modelo;
+    private String gbram;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+
+        return modelo;
+    }
+
+    public void setModelo(String marca) {
+
+        this.modelo = marca;
+    }
+
+    public String getGbram() {
+        return gbram;
+    }
+
+    }
